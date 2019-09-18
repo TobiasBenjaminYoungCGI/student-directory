39389976ea80ef3806f0ba36ff7d8f817d6c6c79
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-09-18 15:36:54 +0100

    add students

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 #first put the students into an array
-students = [
- {name: "Henry Lecter", cohort: :november},
- {name: "Luke Vader", cohort: :november},
- {name: "The Troubled Wicken of the Southwest", cohort: :november},
- {name: "Disruptinator", cohort: :november},
- {name: "Frederic Kreuger", cohort: :november},
- {name: "The Chuckler", cohort: :november},
- {name: "Norma Bates", cohort: :november},
- {name: "BoJo", cohort: :november},
- {name: "The Dust Man", cohort: :november}
-]
+# students = [
+#  {name: "Henry Lecter", cohort: :november},
+#  {name: "Luke Vader", cohort: :november},
+#  {name: "The Troubled Wicken of the Southwest", cohort: :november},
+#  {name: "Disruptinator", cohort: :november},
+#  {name: "Frederic Kreuger", cohort: :november},
+#  {name: "The Chuckler", cohort: :november},
+#  {name: "Norma Bates", cohort: :november},
+#  {name: "BoJo", cohort: :november},
+#  {name: "The Dust Man", cohort: :november}
+# ]
 def print_header
   puts "The Students of Villains Academy"
   puts "-------------------"
@@ -25,6 +25,27 @@ def print_footer(names)
   puts "Overall, we have #{names.count}"
 end
 
+
+
+
+def input_students
+  puts "please enter student names"
+  puts "To finish hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
