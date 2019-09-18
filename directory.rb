2107ca50d5b207cb0927ce0dc7b6a697bb510dd4
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-09-18 15:05:40 +0100

    directory update

--- directory.rb
+++ directory.rb
@@ -10,11 +10,21 @@ students = [
  "BoJo",
  "The Dust Man"
 ]
-#first print the list of students
-puts "THe Students of Villains Academy"
-puts "-------------------"
-students.each do |student|
-  puts students
+def print_header
+  puts "The Students of Villains Academy"
+  puts "-------------------"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count}"
+end
+
+print_header
+print(students)
+print_footer(students)
