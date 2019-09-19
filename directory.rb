66ee9524411aeb6a34276a8437ec17a410b84a11
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-09-19 16:56:03 +0100

    interactive menu + old changes

--- directory.rb
+++ directory.rb
@@ -4,44 +4,53 @@ def print_header
 end
 
 def print(students)
-  cohort_list = []
-  students.each do |student|
-    cohort = student[:cohort].to_s
-    if !(cohort_list.include?(cohort))
-      cohort_list.push(cohort)
-    end
-  end
-  cohort_list.each do |cohort|
-    puts "Students from the #{cohort} cohort:"
-    students.each do |student|
-      if student[:cohort].to_s.include?(cohort)
-        puts student[:name]
-      end
-    end
-  end
-end
-  # t = 0
-  # i = 0
-  #
-  # students.each do |student|
-  #   # while i < students.length
-  #   # if (student[:name])[0] == "s" <- Exercise 2, filter by first letter
-  #   # if (student[:name]).length < 12
-  #   puts "
-  #   #{i += 1}.
-  #   Student Name: #{student[:name]}
-  #   Cohort:#{student[:cohort]}
-  #   Hobby: #{student[:hobbies]}
-  #   Birth Place: #{student[:birthPlace]}
-  #   Height: #{student[:height]}
-  #   "
-    # puts "#{t += 1} #{(students[i][:name])}"
-    # end
-    # i += 1
+#   cohort_list = []
+#   students.each do |student|
+#     cohort = student[:cohort].to_s
+#     if !(cohort_list.include?(cohort))
+#       cohort_list.push(cohort)
+#     end
+#   end
+#   cohort_list.each do |cohort|
+#     puts "Students from the #{cohort} cohort:"
+#     students.each do |student|
+#       if student[:cohort].to_s.include?(cohort)
+#         puts student[:name]
+#       end
+#     end
+#   end
+# end
+  t = 0
+  i = 0
 
+  students.each do |student|
+    # while i < students.length
+    # if (student[:name])[0] == "s" <- Exercise 2, filter by first letter
+    # if (student[:name]).length < 12
+    puts "
+    #{i += 1}.
+    Student Name: #{student[:name]}
+    Cohort:#{student[:cohort]}
+    Hobby: #{student[:hobbies]}
+    Birth Place: #{student[:birthPlace]}
+    Height: #{student[:height]}
+    "
+    # puts "#{t += 1} #{(students[i][:name])}"
+    end
+    i += 1
+  end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} students"
+  if names.count == 0
+    puts "Strange, no students"
+    return
+  end
+  string = "Overall, we have #{names.count} student"
+  if names.count > 1
+    ess = "s"
+    string += ess
+  end
+  puts string
 end
 
 def input_students
@@ -74,14 +83,39 @@ def input_students
       birthPlace: birth,
       height: height
       }
-      puts "Now we have #{students.count} students"
+      string = "Now we have #{students.count} students"
+      if students.count > 1
+        ess = "s"
+        string += ess
+      end
+      puts string
       name = gets.strip
     end
   students
 end
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2.Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, ask again"
+    end
+  end
+end
+
 
 students = input_students
-print_header
-print(students)
-print_footer(students)
